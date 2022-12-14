73b4a77c501e46347a0a357c289c2ec9332ee610
Rafael Goncalves <rsgoncalves@gmx.com>
2022-12-13 20:25:37 -0500

    Add simple test / example usage of onto2table

--- ontoutils/test/test_onto2table.py
+++ ontoutils/test/test_onto2table.py
@@ -1,6 +1,11 @@
 from ontoutils.onto2table import Onto2Table
 
-df = Onto2Table.generate_ontology_table(ontology_file="http://purl.obolibrary.org/obo/cl/releases/2022-09-15/cl.owl",
-                                        output_file="cl.csv",
-                                        use_reasoning=True)
+df = Onto2Table.generate_ontology_table(ontology_file="http://purl.obolibrary.org/obo/hp/releases/2022-06-11/hp.owl",
+                                        output_file="hpo.csv",
+                                        use_reasoning=False)
 print(df.to_string())
+
+# when using reasoning it may be necessary to increase the max Java heap space
+# that the reasoner (provided by owlready) can use, which can be done like so:
+# import owlready2
+# owlready2.reasoning.JAVA_MEMORY = 16000
